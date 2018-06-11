c78daa3b2b8f9986433f8faed591d23419aafc86
PotassiumIodide <38805329+PotassiumIodide@users.noreply.github.com>
2018-06-11 12:29:44 +0530

    Update ver2(Accepted).py

--- arrays/findDuplicatesInarray/ver2(Accepted).py
+++ arrays/findDuplicatesInarray/ver2(Accepted).py
@@ -3,6 +3,9 @@ class Solution:
     # @return an integer
     def repeatedNumber(self, A):
         A = list(A)
+        #since the elements of the tuples are
+        #in range of length of tuple, all elements
+        #if taken as index is valid.
         for i in range(len(A)):
             if(A[abs(A[i])] >= 0):
                 A[abs(A[i])] = -A[abs(A[i])]
