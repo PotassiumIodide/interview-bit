b4d7654fbcb01f431aa360beffd1200b2966ec39
PotassiumIodide <38805329+PotassiumIodide@users.noreply.github.com>
2018-06-11 13:14:14 +0530

    Create MergeInterval.py

---
+++ arrays/MergeInterval.py
@@ -0,0 +1,20 @@
+class Solution:
+    # @param intervals, a list of Intervals
+    # @param new_interval, a Interval
+    # @return a list of Interval
+    def insert(self, intervals, new_interval):
+        intervals.append(new_interval)
+        intervals.sort(key=lambda x: x.start)
+
+        merged = []
+        for interval in intervals:
+            # if the list of merged intervals is empty or if the current
+            # interval does not overlap with the previous, simply append it.
+            if not merged or merged[-1].end < interval.start:
+                merged.append(interval)
+            else:
+            # otherwise, there is overlap, so we merge the current and previous
+            # intervals.
+                merged[-1].end = max(merged[-1].end, interval.end)
+        
+        return merged
