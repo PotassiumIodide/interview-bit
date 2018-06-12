e56c26f63fbfabbb7232345ac6d1f22de67b18ae
PotassiumIodide <38805329+PotassiumIodide@users.noreply.github.com>
2018-06-12 13:16:21 +0530

    Update setMatrixZero.py

--- arrays/setMatrixZero.py
+++ arrays/setMatrixZero.py
@@ -1,19 +1,20 @@
- nrows = len(matrix)
-        ncols = len(matrix[0])
+class Solution:
+     # @param A : list of list of integers
+    # @return the same list modified
+    def setZeroes(self, A):
+        nrows = len(A)
+        ncols = len(A[0])
+        for x, row in enumerate(A):
+            if 0 in row:
+                for y, n in enumerate(row):
+                    if n == 0: 
+                        A[0][y] = None
+                    if n != 0 or x != 0:
+                        A[x][y] = 0
         for i in range(nrows):
+            i_ = nrows - i -1
             for j in range(ncols):
-                if matrix[i][j]==0:
-                    matrix[i][j]='0'
-                else:
-                    pass
-        for i in range(nrows):
-            for j in range(ncols):
-                if matrix[i][j]=='0':
-                    matrix[i][j]=0
-                    for k in range(nrows):
-                        if (matrix[k][j] != '0'):
-                            matrix[k][j] = 0
-                    
-                    for n in range(ncols):
-                        if (matrix[i][n] != '0'):
-                            matrix[i][n] = 0
+                if A[0][j] is None:
+                    A[i_][j] = 0
+                            
+        return A
