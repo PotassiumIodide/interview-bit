348e79b43a95257ed677d199ee00ab674d05efa5
PotassiumIodide <38805329+PotassiumIodide@users.noreply.github.com>
2018-06-15 01:58:57 +0530

    Create RotatedSorted.py

---
+++ Binary Search/RotatedSorted.py
@@ -0,0 +1,50 @@
+def binSearch(self,A, B) :
+        """
+        Binary Search Algorithm
+        """
+        low = 0
+        high = len(A) - 1
+        result = -1
+        while low <= high :
+            mid = (low + high)/2
+            if A[mid] == B :
+                return mid
+            elif B > A[mid] :
+                low = mid + 1
+            else :
+                high = mid - 1
+        return -1
+
+def findMin(self,A):
+        """
+        Find the index of minimum element in a rotated array
+        """
+        low = 0
+        high = len(A) - 1
+        N = len(A)
+        while low <= high :
+            mid = (low + high)/2
+            next = (mid + 1)%N
+            prev = (mid + N - 1)%N
+            if A[low] < A[high] :
+                return low
+            elif A[mid] <= A[next] and A[mid] <= A[prev] :
+                return mid
+            elif A[mid] <= A[high] :
+                high = mid - 1
+            elif A[mid] >= A[low] :
+                low = mid + 1
+
+def search(self,A, B) :
+        minIndex = self.findMin(A)
+        if minIndex == 0 or minIndex == len(A) - 1 :
+            return self.binSearch(A, B)
+        else :
+            res1 = self.binSearch(A[:minIndex], B)
+            res2 = self.binSearch(A[minIndex:], B)
+            if res1 == -1 and res2 == -1 :
+                return -1
+            if res1 == -1 :
+                return res2 + minIndex
+            elif res2 == -1 :
+                return res1
