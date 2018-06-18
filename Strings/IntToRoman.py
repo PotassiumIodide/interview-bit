e03028a938d57d439a1f19aa73361f838ca83d19
PotassiumIodide <38805329+PotassiumIodide@users.noreply.github.com>
2018-06-18 15:41:00 +0530

    Update IntToRoman.py

--- Strings/IntToRoman.py
+++ Strings/IntToRoman.py
@@ -3,12 +3,12 @@ class Solution:
     # @return a strings
     def intToRoman(self, A):
         value = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-        numerals = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+        numericals ={1000:'M',900:'CM',500:'D',400:'CD',100:'C',90:'XC',50:'L',40:'XL',10:'X',9:'IX',5:'V',4:'IV',1:'I'}
         res , i = "", 0
         if  A < 1 or A > 3999 :
             return res
         while A :
-            res += (A//value[i])*numerals[i]
+            res += (A//value[i])*numericals[value[i]]
             A %= value[i]
             i += 1
         return res
